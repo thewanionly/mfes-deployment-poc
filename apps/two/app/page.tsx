5838f9ecdfedec98522ea49784d7aecfa1b4abe4
Elloani Ross Pitogo <pelloani@gmail.com>
2025-12-10 18:47:45 +0800

    add color to the button

--- apps/two/app/page.tsx
+++ apps/two/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
-        <Button appName='two'>Two</Button>
+        <Button appName='two' className='border px-4 py-2 bg-amber-700'>
+          Two
+        </Button>
       </main>
     </div>
   )
