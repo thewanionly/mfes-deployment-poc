7ae35251a3cac9bee83f63cbe7f4baa7b5d4bdfe
Elloani Ross Pitogo <pelloani@gmail.com>
2025-12-15 18:23:34 +0800

    add description in products and orders

--- apps/products/app/page.tsx
+++ apps/products/app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Products</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sint dicta aliquid ipsum magni
+          facilis sapiente fugit dolor eveniet enim ratione eaque nostrum, in perferendis placeat
+          eligendi inventore eos. Dolorem!
+        </p>
         <Button appName='products' className={styles.secondary}>
           Learn more
         </Button>
