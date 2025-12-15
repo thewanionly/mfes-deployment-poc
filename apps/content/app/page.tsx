d9bf974e80f8eaee39855cf58ba354c0d29e601a
Elloani Ross Pitogo <pelloani@gmail.com>
2025-12-15 19:52:07 +0800

    add qa env

--- apps/content/app/page.tsx
+++ apps/content/app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Content</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sint dicta aliquid ipsum magni
+          facilis sapiente fugit dolor eveniet enim ratione eaque nostrum, in perferendis placeat
+          eligendi inventore eos. Dolorem!
+        </p>
         <Button appName='content' className={styles.secondary}>
           Learn more
         </Button>
