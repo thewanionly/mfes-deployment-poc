65b31ec1d8664c2a1cef944faf095713d979d2b5
Elloani Ross Pitogo <pelloani@gmail.com>
2025-12-16 20:59:11 +0800

    make prod depend on qa

--- apps/customers/app/page.tsx
+++ apps/customers/app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Customers</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sint dicta aliquid ipsum magni
+          facilis sapiente fugit dolor eveniet enim ratione eaque nostrum, in perferendis placeat
+          eligendi inventore eos. Orders!
+        </p>
         <Button appName='customers' className={styles.secondary}>
           Learn more
         </Button>
